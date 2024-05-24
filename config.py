956dbe43d94ba118fe6308b83b5a839ba2d5df05
Sevenxyz <136231357+ISevenxyz@users.noreply.github.com>
2024-05-24 15:27:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-API_HASH = os.getenv("API_HASH")
-API_ID = int(os.getenv("API_ID"))
-MONGO_URI = os.getenv("MONGO_URI")
-PREFIX = os.getenv("PREFIX")
-LOG_CHAT = int(os.getenv("LOG_CHAT"))
+API_HASH = os.getenv("ad9e119acbfc9de527e1da32fae2a866")
+API_ID = int(os.getenv("24679387"))
+MONGO_URI = os.getenv("mongodb+srv://iSeven:iSeven123@cluster0.hmuzzak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+PREFIX = os.getenv(".")
+LOG_CHAT = int(os.getenv("-1002150789287"))
